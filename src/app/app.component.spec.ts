07e5282a2be19a26960d57f719ccf02149c31b50
William Welling <8352733+wwelling@users.noreply.github.com>
2022-06-02 23:09:08 -0500

    remove default app component template

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('Vitro-angular');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('Vitro-angular app is running!');
-  });
 });
