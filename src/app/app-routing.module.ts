c9413d11dfc9740b2e938349de6082cd11a8b424
SoulsMark <markthefolkin@gmail.com>
2022-06-16 16:21:40 +0200

    initial login form implementation

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginComponent } from "./components/login/login.component";
 
 const routes: Routes = [
   {
@@ -8,6 +9,10 @@ const routes: Routes = [
     canActivate: [],
     loadChildren: () =>
       import('./dashboard/dashboard.module').then((m) => m.DashboardModule),
+  },
+  {
+    path: 'login',
+    component: LoginComponent,
   },
   {
     path: '**',
