fe24869f390c6f0f30b4b40b5803f3612847d9f5
SoulsMark <markthefolkin@gmail.com>
2022-06-21 15:40:26 +0200

    fix: NullInjectorError: No provider for FormBuilder!

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -8,7 +8,7 @@ import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
 import { LoginComponent } from './components/login/login.component';
 import { AngularMaterialModule } from "./angular.material.module";
-import { ReactiveFormsModule } from "@angular/forms";
+import {ReactiveFormsModule} from "@angular/forms";
 
 @NgModule({
   declarations: [AppComponent, LoginComponent],
